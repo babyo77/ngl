4c97cd3a5fb91720be9d809dbaa0c71cdd8e0136
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-04 07:54:46 +0530

    Update Tabs.tsx

--- src/components/Account/Tabs.tsx
+++ src/components/Account/Tabs.tsx
@@ -57,7 +57,7 @@ onSnapshot(unseenMessagesQuery, (snapshot) => {
       <TabsList className="w-full bg-transparent py-2.5 z-10 fixed top-0 bg-white h-fit items-center justify-between px-4 border-b">
        
        {unseenMessagesCount > 0 &&(
-         <h1 className=" font-extrabold text-[#EC1187]">{unseenMessagesCount > 99 && "99 + Unread"} {unseenMessagesCount < 99 &&(`${unseenMessagesCount} unread`)}</h1>
+         <h1 className=" font-extrabold text-[#EC1187]">{unseenMessagesCount > 99 && "99+ Unread"} {unseenMessagesCount < 99 &&(`${unseenMessagesCount} unread`)}</h1>
        )} 
         {unseenMessagesCount<=0 &&(
           <h1 className=" font-extrabold text-[#EC1187] h-8 w-8"></h1>
