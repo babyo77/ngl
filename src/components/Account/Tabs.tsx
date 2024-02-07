191770b9a70ea6f87ae3824d50dff16b78d36b8a
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-07 11:29:25 +0530

    Update Tabs.tsx

--- src/components/Account/Tabs.tsx
+++ src/components/Account/Tabs.tsx
@@ -41,6 +41,19 @@ export function Tab() {
 
   const enableNotifications = async () => {
     if ("Notification" in window) {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+          .register("/firebase-messaging-sw.js")
+          .then((registration) => {
+            console.log(
+              "Service Worker registered with scope:",
+              registration.scope
+            );
+          })
+          .catch((error) => {
+            console.error("Service Worker registration failed:", error);
+          });
+      }
       const permission = await Notification.requestPermission();
       if (permission === "granted") {
         const token = await getToken(messaging, {
