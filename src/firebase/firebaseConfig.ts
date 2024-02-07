1d71186d191029e1f8e84c37be304165d431f8f1
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-07 15:15:03 +0530

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore, collection } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getMessaging } from "firebase/messaging";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC3UYt7dxYgR-8T1DgdZT86pwi5JmKLF2Y",
+  authDomain: "ngl-drx-prod.firebaseapp.com",
+  projectId: "ngl-drx-prod",
+  storageBucket: "ngl-drx-prod.appspot.com",
+  messagingSenderId: "162610520917",
+  appId: "1:162610520917:web:e039ca4f4c659eeafa05dc",
+  measurementId: "G-C7VFF3RJ2P",
 };
 
 const app = initializeApp(firebaseConfig);
