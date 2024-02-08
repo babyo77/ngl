aacd56afe3cd00d088e7b2743c0fde0e11086d66
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-08 20:55:22 +0530

    Update Delete .tsx

--- src/components/Account/Delete .tsx
+++ src/components/Account/Delete .tsx
@@ -1,67 +1,82 @@
-import { apiUrl } from "@/API/api"
+import { apiUrl } from "@/API/api";
 import {
-    AlertDialog,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-  import { Button } from "@/components/ui/button"
-import { useState } from "react"
-import { Loader } from "../Loaders/Loader"
-  
-  export function DeleteDialog() {
-const [isLoading,setLoading] = useState<boolean>(false)
-    const deleteUser = async()=>{
-         try {
-            setLoading(true)
-            const authToken = ""
-            const data = {
-               token:authToken,
-            }
-            const startDelete = await fetch(`${apiUrl}/api/del`,{
-               method:"post",
-               headers:{
-                   "content-type":"application/json"
-               },
-               body: JSON.stringify(data)
-            })
-            if(startDelete.ok){
-               window.location.reload()
-            }
-         } catch (error) {
-            alert((error as {message:string}).message)
-         }
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Loader } from "../Loaders/Loader";
+import { auth } from "@/firebase/firebaseConfig";
+
+export function DeleteDialog() {
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const deleteUser = async () => {
+    try {
+      setLoading(true);
+      const authToken = await auth.currentUser?.getIdToken();
+      const data = {
+        token: authToken,
+      };
+      const startDelete = await fetch(`${apiUrl}/api/del`, {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (startDelete.ok) {
+        window.location.reload();
+      }
+    } catch (error) {
+      alert((error as { message: string }).message);
     }
+  };
 
-    return (
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant={"secondary"} className="text-[1rem] mt-3 text-red-500 font-bold rounded-2xl py-6">Delete account</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent className="w-[87dvw] rounded-2xl">
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel className="text-[1rem] mt-3 font-bold rounded-2xl py-[1.4rem]">Cancel</AlertDialogCancel>
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant={"secondary"}
+          className="text-[1rem] mt-3 text-red-500 font-bold rounded-2xl py-6"
+        >
+          Delete account
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="w-[87dvw] rounded-2xl">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="text-[1rem] mt-3 font-bold rounded-2xl py-[1.4rem]">
+            Cancel
+          </AlertDialogCancel>
           {isLoading ? (
-              <Button disabled className="text-[1rem] bg-red-500 mt-3 font-bold rounded-2xl py-6"><Loader/></Button>
-          ):(
-            <Button onClick={deleteUser} className="text-[1rem] bg-red-500 mt-3 font-bold rounded-2xl py-6">Delete account</Button>
+            <Button
+              disabled
+              className="text-[1rem] bg-red-500 mt-3 font-bold rounded-2xl py-6"
+            >
+              <Loader />
+            </Button>
+          ) : (
+            <Button
+              onClick={deleteUser}
+              className="text-[1rem] bg-red-500 mt-3 font-bold rounded-2xl py-6"
+            >
+              Delete account
+            </Button>
           )}
-          
-
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    )
-  }
-  
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
