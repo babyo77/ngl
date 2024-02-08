4950c076dcede31cb0829ca3367763cafe950482
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-08 22:02:36 +0530

    Update DrawerCards.tsx

--- src/components/Account/DrawerCards.tsx
+++ src/components/Account/DrawerCards.tsx
@@ -56,11 +56,7 @@ export function DrawerCard({
     if (twitterRef.current === null) return;
 
     twitterRef.current.classList.replace("hidden", "flex");
-    firstRender &&
-      (await toBlob(twitterRef.current).then(() => {
-        twitterRef.current?.classList.replace("flex", "hidden"),
-          setFirstRender(false);
-      }));
+    firstRender && (await toBlob(twitterRef.current), setFirstRender(false));
 
     toBlob(twitterRef.current, {
       cacheBust: true,
