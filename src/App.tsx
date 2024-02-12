33303d3e69c5280bcf30cfb7053dc71065f1073c
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-13 05:20:21 +0530

    Update Container.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,44 +10,64 @@ function App() {
   const [Found, setFound] = useState<boolean>(false);
   const [isLoading, setLoading] = useState<boolean>(true);
   const [details, setUser] = useState<user>();
+  const [randomGradient, setRandomGradient] = useState<string>();
+
+  const randomBg = () => {
+    const random = Math.floor(Math.random() * 3);
+    const gradient = [
+      "bg-gradient-to-br from-blue-500 to-purple-800",
+      "bg-gradient-to-br from-[#EC1187] to-[#FF8D10]",
+      "bg-gradient-to-br from-pink-500 via-purple-600 to-purple-900",
+    ];
+    setRandomGradient(gradient[random]);
+  };
+
   useEffect(() => {
     const userExist = async () => {
       const user = await fetch(`${apiUrl}/user/${username || "tanmay"}`);
-      const details: user  = await user.json();
+      const details: user = await user.json();
       setUser(details);
       user.ok
         ? (setFound(true), setLoading(false))
         : (setFound(false), setLoading(false));
     };
     userExist();
+    randomBg();
   }, [username]);
   if (isLoading) {
     return (
       <>
-          <div className="absolute top-0 z-[-2] h-full w-screen bg-gradient-to-br from-[#EC1187] to-[#FF8D10]"></div>
-          <div className="flex justify-center pt-12 ">
-            <Temp/>
-          </div>
-        </>
+        <div
+          className={`absolute top-0 z-[-2] h-full w-screen ${randomGradient}`}
+        ></div>
+        <div className="flex justify-center pt-12 ">
+          <Temp />
+        </div>
+      </>
     );
   }
   return (
     <>
       {Found ? (
         <>
-          <div className="absolute top-0 z-[-2] h-full w-screen bg-gradient-to-br from-[#EC1187] to-[#FF8D10]"></div>
+          <div
+            className={`absolute top-0 z-[-2] h-full w-screen ${randomGradient}`}
+          ></div>
           <div className="flex justify-center pt-12 ">
-            <Container userDetails={details}/>
+            <Container userDetails={details} />
           </div>
         </>
       ) : (
         <>
-          <div className="absolute top-0 z-[-2] h-full w-screen bg-gradient-to-br from-[#EC1187] to-[#FF8D10]"></div>
+          <div
+            className={`absolute top-0 z-[-2] h-full w-screen ${randomGradient}`}
+          ></div>
           <div className="flex justify-center items-center h-dvh">
-            <h1 onClick={()=>window.location.href = "/"} className="text-2xl cursor-pointer font-bold text-white underline-offset-8 underline">
-              
-Not Found Go back
-
+            <h1
+              onClick={() => (window.location.href = "/")}
+              className="text-2xl cursor-pointer font-bold text-white underline-offset-8 underline"
+            >
+              Not Found Go back
             </h1>
           </div>
         </>
