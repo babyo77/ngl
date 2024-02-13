9bdad4ed67834495dc2ab96834d0f60853d44b37
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-13 06:20:54 +0530

    Update ChangeDetails.tsx

--- src/components/Account/ChangeDetails.tsx
+++ src/components/Account/ChangeDetails.tsx
@@ -1,4 +1,3 @@
-
 import {
   Dialog,
   DialogContent,
@@ -12,13 +11,11 @@ function ChangeDetails() {
   return (
     <Dialog>
       <DialogTrigger>
-        <p
-          className="text-[1rem] w-full bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 font-bold rounded-2xl py-3"
-        >
+        <p className="text-[1rem] w-full bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 font-bold rounded-2xl py-3">
           View account details
         </p>
       </DialogTrigger>
-      <DialogContent className="w-[87vw] rounded-2xl">
+      <DialogContent className="w-[87vw] rounded-2xl top-60">
         <DialogHeader>
           <DialogTitle className=" text-lg font-extrabold">
             Account details
