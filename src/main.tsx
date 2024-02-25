b04d6a7faa74c8d86c17c4e9715a94d54b6afb27
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-25 21:46:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,29 +6,25 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Account from "./components/Account/Account.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-try {
-  const queryClient = new QueryClient();
-  const router = createBrowserRouter([
-    {
-      path: "/:username?",
-      element: <App />,
-    },
-    {
-      path: "/da/account",
-      element: <Account />,
-    },
-    {
-      path: "*",
-      element: <App />,
-    },
-  ]);
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </React.StrictMode>
-  );
-} catch (error) {
-  alert(error);
-}
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {
+    path: "/:username?",
+    element: <App />,
+  },
+  {
+    path: "/da/account",
+    element: <Account />,
+  },
+  {
+    path: "*",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
